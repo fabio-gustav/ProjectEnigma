d15fa9810d92e6c752164cd69907ee3cdd6b9168
Jason <95380767+Jaiwin42@users.noreply.github.com>
2025-05-13 18:49:01 -0500

    Moving Platforms and Elevator
     - Kinda Works
     - Moving platform is good, and can have it's speed adjusted in node inspector.
     - Elevator doesn't have floors yet, but can be controlled to go up and down while on it.
    - I'll work more on it later, just commiting this for now.

--- projectenigma/World Objects/Platforms/StagedMovingPlatform/staged_moving_platform.gd
+++ projectenigma/World Objects/Platforms/StagedMovingPlatform/staged_moving_platform.gd
@@ -1,32 +1,51 @@
 extends Path2D
 
 
-var speed = 2
-var stage = 0
-var loop = false
+@export var speed :float = 1
 var onPlatform = false
+var goBack = false# used for detecting whent the animation finishes so we don't jump around everywhere
 @onready var path = $PathFollow2D
 @onready var animation = $AnimationPlayer
 @onready var platform = $AnimatableBody2D
 
 
+#Note: back and forth isn't "and forth" for this platform, I was just too lazy to change the name
 func _ready():
 	animation.play("back_and_forth")
+	animation.speed_scale = speed
 	animation.pause()
 	
 
-# used if you want a looping path
-func _process(_delta):
+# stupid function
+func _physics_process(delta: float) -> void:
 	if (onPlatform == true && Input.is_action_just_pressed("interact")):
 		if(animation.is_playing()):
 			animation.pause()
 		else:
-			animation.play("back_and_forth")
+			if(Input.is_action_pressed("down")):
+				if(goBack):
+					if(animation.speed_scale < 0):
+						return
+					animation.play("back_and_forth", -1, 1, true)
+					goBack = false
+				else:
+					animation.play("back_and_forth")
+				animation.speed_scale = -speed
+			if(Input.is_action_pressed("up")):
+				if(goBack):
+					if(animation.speed_scale > 0):
+						return
+					animation.play("back_and_forth", -1, 1, false)
+					goBack = false
+				else:
+					animation.play("back_and_forth")
+				animation.speed_scale = speed
 				
-			
-			
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	onPlatform = true
 	
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	onPlatform = false
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	goBack = true;
