250d768db8a6f0deeb867747dc0b1df9bdffdd47
Shane Burke <67300048+fabio-gustav@users.noreply.github.com>
2025-09-09 23:20:39 -0500

    Much Better Animations
    Still need to fix grapple

--- project-enigma/Player/PlayerSprite.cs
+++ project-enigma/Player/PlayerSprite.cs
@@ -6,33 +6,41 @@ public partial class PlayerSprite : Node2D
     
     public AnimatedSprite2D Top;
     public AnimatedSprite2D Bottom;
+    private AnimationPlayer _animationPlayer;
+    private CollisionShape2D _collider;
     private Player _player;
 
     public override void _Ready()
     {
         Top = GetNode<AnimatedSprite2D>("Top");
         Bottom = GetNode<AnimatedSprite2D>("Bottom");
+        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        _collider = GetNode<CollisionShape2D>("../Collider");
         _player = GetParent<Player>();
     }
 
     public void PlayAnimation(string anim)
     {
-        Top.Play(anim);
-        Bottom.Play(anim);
+        _animationPlayer.Play(anim);
     }
     
     public override void _PhysicsProcess(double delta)
     {
-        if (_player.Velocity.X < 0.0f && !Top.FlipH)
+        if (_player.Velocity.X < 0.0f)
         {
-            Top.FlipH = true;
-            Bottom.FlipH = true;
+            //Top.FlipH = true;
+            //Bottom.FlipH = true;
+            _player.Scale = new Vector2(1, -1);
+            _player.RotationDegrees = 180;
+            //GD.Print("PlayerScale: " + _player.Scale);
             
         }
-        else if (_player.Velocity.X >= 0.0f && Top.FlipH)
+        else
         {
-            Top.FlipH = false;
-            Bottom.FlipH = false;
+            //Top.FlipH = false;
+            //Bottom.FlipH = false;
+            _player.Scale = new Vector2(1, 1);
+            _player.RotationDegrees = 0;
             
         }
     }
