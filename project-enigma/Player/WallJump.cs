fa6e778ac30886d3d90038e7fe2e08b2a6a1bc56
TheRamCom <95380707+TheRamCom@users.noreply.github.com>
2025-09-18 19:42:43 -0500

    Added New State - Wall Jump

---
+++ project-enigma/Player/WallJump.cs
@@ -0,0 +1,111 @@
+using Godot;
+using System;
+
+public partial class WallJump : State
+{
+    [Export] public State GrappleState { get; set; } = null;
+    [Export] public State ParryState { get; set; } = null;
+    [Export] public State FallState { get; set; } = null;
+
+    private float _jumpVelocity = 0.0f;
+    private float _jumpGravity = 0.0f;
+    private float _ridingJumpVelocity = 0.0f;
+    private float _ridingJumpGravity = 0.0f;
+
+    //Returns 0: Not Colliding, 1: Left, 2: Right
+    private int LeftOrRight()
+    {
+        bool right = GetNode<RayCast2D>("WallCheck/RightCheck").IsColliding();
+        bool left = GetNode<RayCast2D>("WallCheck/Leftheck").IsColliding();
+        if (left)
+        {
+            return 1;
+        }
+        else
+        {
+            if (right)
+            {
+                return 2;
+            }
+        }
+
+        return 0;
+    }
+    
+    public override void Init()
+    {
+        _jumpVelocity = ((2.0f * Player.JumpHeight) / Player.RisingJumpTime) * -1.0f;
+        _jumpGravity = ((-2.0f * Player.JumpHeight) / (Player.RisingJumpTime * Player.RisingJumpTime)) * -1.0f;
+        _ridingJumpVelocity = ((2.0f * Player.RideJumpHeight) / Player.RideRisingJumpTime) * -1.0f;
+        _ridingJumpGravity = ((-2.0f * Player.RideJumpHeight) / (Player.RideRisingJumpTime * Player.RideRisingJumpTime)) * -1.0f;
+
+    }
+    
+
+    public override void Enter()
+    {
+        //Player.ParryAvailable = true
+        int side = LeftOrRight();
+        if (side == 0)
+        {
+            GD.Print("Does not collide");
+        }
+
+        const float AngleChange = (float)Math.PI / 4;
+        if (side == 1)
+        {
+            Player.Velocity = new Vector2(Player.Velocity.X, _ridingJumpVelocity).Rotated(-AngleChange);
+        }
+        if (side == 2)
+        {
+            Player.Velocity = new Vector2(Player.Velocity.X, _jumpVelocity).Rotated(AngleChange);;
+        }
+
+        Player._jumpAvailable = false;
+
+        Player.PlayerSprite.PlayAnimation("Jump");
+    }
+
+    public override void Exit()
+    {
+        //Player.Velocity = new Vector2(Player.Velocity.X, 0.0f);
+        _jumpGravity = ((-2.0f * Player.JumpHeight) / (Player.RisingJumpTime * Player.RisingJumpTime)) * -1.0f;
+        _ridingJumpGravity = ((-2.0f * Player.RideJumpHeight) / (Player.RideRisingJumpTime * Player.RideRisingJumpTime)) * -1.0f;
+    }
+
+    public override State ProcessInput(InputEvent @event)
+    {
+        //fix this name later
+        //GD.Print(Player.GrappleCheck());
+        if (Player.GrappleCheck() && @event.IsActionPressed("grapple"))
+        {
+            return GrappleState;
+        }
+
+        return null;
+    }
+
+    public override State PhysicsUpdate(double delta)
+    {
+        //float.Lerp(Player.Velocity.X, Mathf.Clamp((Player.AirSpeed*GetInput())+Player.Velocity.X,-Player.Speed,Player.Speed), Player.Acceleration)
+        Player.Velocity = new Vector2(float.Lerp(Player.Velocity.X, (Player.AirSpeed*GetInput())+Player.Velocity.X, Player.Acceleration*(float)delta), Player.Velocity.Y + (float)(_jumpGravity * delta));
+
+        //Acceleration needs to be fixed, also I'm getting rid of AirResistance because it is dumb and stupid
+        
+        if (Player.Velocity.Y >= 0.0f)
+        {
+            return FallState;
+        }
+
+        if (!Input.IsActionPressed("jump"))
+        {
+
+            _jumpGravity = ((-6.0f * Player.JumpHeight) / (Player.RisingJumpTime * Player.RisingJumpTime)) * -1.0f;
+            
+            //We achieve jump cutting by just tripling gravity. We should make this better at some point
+            
+        }
+
+        return null;
+    }
+}
